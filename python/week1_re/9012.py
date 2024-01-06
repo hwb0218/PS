c10d4014215dbda4577f79776fdf29cba0677f81
FEWoobIn <hwb0218@gmail.com>
2024-01-06 21:20:35 +0900

    9012 괄호

---
+++ python/week1_re/9012.py
@@ -0,0 +1,38 @@
+from sys import stdin
+
+tc = int(stdin.readline())
+
+# 풀이1
+for i in range(tc):
+    s = stdin.readline().rstrip()
+    stack = []
+    flag = False
+    for char in s:
+        if char == '(':
+            stack.append(char)
+        if char == ')':
+            if stack:
+                stack.pop()
+            else:
+                flag = True
+                break
+    if flag or stack:
+        print('NO')
+    elif not flag and not stack:
+        print('YES')
+
+# 풀이2
+for i in range(tc):
+    s = stdin.readline().rstrip()
+    stack = []
+
+    for char in s:
+        if stack and stack[-1] == '(' and char == ')' 
+            stack.pop()
+            continue
+        stack.append(char)
+
+    if stack:
+        print('NO')
+    else:
+        print('YES')
